fcc2f109ea26ec1814ddda7a7f678280ab3f886b
Mduduzi Frederick Dube <tungatadube@gmail.com>
2017-10-06 15:28:01 +0200

    Added the Welcome screen

--- TheScienceHub_project/the_science_hub_project/home_page/views.py
+++ TheScienceHub_project/the_science_hub_project/home_page/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def homepage(request):
+    string_to_dispaly = "Welcome"
+    title = "The Science Hub "
+    context = {"string_to_dispaly": string_to_dispaly, "title": title}
+    return render(request, "home_page/base.html", context)
